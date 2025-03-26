bcb95bf43f9c6ecf438df6abc3c2463e8751d896
Semia Guesmi <semiaguesmi@Semias-MacBook-Pro.local>
2025-03-26 12:49:55 +0100

    second c

--- types/byte.go
+++ types/byte.go
@@ -2,8 +2,17 @@ package types
 
 import "fmt"
 
-func Run() {
+func Byte_8() {
 	var a byte = 65
 	fmt.Println(a)
 	fmt.Println(string(a))
+}
+func Byte_string() {
+	str := "ameni"
+	byte_str := []byte(str) // Convert string to byte slice
+	fmt.Println(byte_str)
+	byte_str[4] = 'M'
+	fmt.Println(byte_str)
+	fmt.Println(string(byte_str))
+
 }
