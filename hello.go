37b44b2d1ab744203db646715074ed4d0a29b35d
Semia Guesmi <semiaguesmi@Semias-MacBook-Pro.local>
2025-03-26 12:03:44 +0100

    Initial commit

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"LEARN_GO/types"
+	"fmt"
+)
+import "rsc.io/quote"
+
+func main() {
+	fmt.Println(quote.Go())
+	types.Run()
+}
---
+++ types/byte.go
@@ -0,0 +1,9 @@
+package types
+
+import "fmt"
+
+func Run() {
+	var a byte = 65
+	fmt.Println(a)
+	fmt.Println(string(a))
+}
